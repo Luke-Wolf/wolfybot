2ae2957e436e244a7a960daab3c7c967798a71cb
Luke-Wolf <lukewolf101010@gmail.com>
2014-12-01 23:51:25 -0700

    Implement IRCMessage

--- WolfyBot.Core/IRCMessage.cs
+++ WolfyBot.Core/IRCMessage.cs
@@ -13,6 +13,9 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
 
 namespace WolfyBot.Core
 {
@@ -27,17 +30,50 @@ namespace WolfyBot.Core
 		public IRCMessage (String ircMessage)
 		{
 			TimeStamp = DateTime.Now;
-			//TODO: write IRC Parser
+
+			//If an IRC Message starts with a colon the first string before
+			//the space is the prefix
+			int prefixEnd = -1;
+			if (ircMessage.StartsWith (":", StringComparison.Ordinal)) {
+				prefixEnd = ircMessage.IndexOf (" ", StringComparison.Ordinal);
+				Prefix = ircMessage.Substring (1, prefixEnd - 1);
+			}
+
+			//Get the Trailing Parameters if they exist as a string literal
+			//Trailing Parameters are demarkated by a space and a colon at the
+			//end of the normal parameter set
+			int trailingStart = ircMessage.IndexOf (" :", StringComparison.Ordinal);
+			if (trailingStart >= 0) {
+				TrailingParameters = ircMessage.Substring (trailingStart + 2);
+			} else {
+				trailingStart = ircMessage.Length;
+			}
+
+			//pull the command and parameters out of the IRC Message by pulling everything
+			//between the prefix and the trailing parameters, The Command is the first item,
+			//and everything else if it exists is the parameter to that command
+			String[] commandAndParameters = ircMessage.Substring (prefixEnd + 1, trailingStart - prefixEnd - 1).Split (' ');
+			Command = commandAndParameters [0];
+			if (commandAndParameters.Length > 1) {
+				Parameters = commandAndParameters.Skip (1).ToList ();
+				//If Parameters contains a channel pull it out
+				foreach (var item in Parameters) {
+					if (item.Contains ("#")) {
+						Channel = item;
+						break;
+					}
+				}
+			}
 		}
 
 		//Copy Constructor
 		public IRCMessage (IRCMessage other)
 		{
 			TimeStamp = other.TimeStamp;
-			Prefix = other.Prefix;
-			Command = other.Command;
-			Parameters = other.Parameters;
-			TrailingParameters = other.TrailingParameters;
+			Prefix = String.Copy (other.Prefix);
+			Command = String.Copy (other.Command);
+			Parameters = new List<String> (other.Parameters);
+			Channel = String.Copy (other.Channel);
 		}
 
 		#endregion
@@ -46,15 +82,20 @@ namespace WolfyBot.Core
 
 		public String ToIRCString ()
 		{
-			//TODO: Implement Serialization to IRC Protocol
-			return "";
+			return String.Format (":{0} {1} {2} :{3}", Prefix,
+				Command, Parameters.ToString ().Replace (",", " "), TrailingParameters);
 		}
 
 		public String ToLogString ()
 		{
+			return String.Format ("{0} {1} {2} {3} {4}", TimeStamp, Prefix,
+				Command, Parameters.ToString ().Replace (",", " "), TrailingParameters);
+		}
 
-			//TODO: Implement Serialization to logging format
-			return "";
+		public String ToString ()
+		{
+			return String.Format ("TimeStamp: {0}, IRC Message: ':{1} {2} {3} :{4}'",
+				TimeStamp, Prefix, Command, Parameters.ToString ().Replace (",", " "), TrailingParameters);
 		}
 
 		#endregion
@@ -71,7 +112,7 @@ namespace WolfyBot.Core
 			set;
 		}
 
-		public String Parameters {
+		public List<String> Parameters {
 			get;
 			set;
 		}
@@ -82,6 +123,11 @@ namespace WolfyBot.Core
 		}
 
 		public DateTime TimeStamp {
+			get;
+			set;
+		}
+
+		public String Channel {
 			get;
 			set;
 		}
